738af33cc7a95820372e383c463b043905066239
Hoang Trung Anh <anh.hoang04k22@hcmut.edu.vn>
2025-12-14 17:27:25 +0700

    feat: Add Notification Detail page and enhance BillingGroupSection with navigation; update Notifice layout

--- mobile-app/app/(tabs)/notifice/_layout.tsx
+++ mobile-app/app/(tabs)/notifice/_layout.tsx
@@ -4,6 +4,7 @@ export default function NotificeLayout() {
     return (
         <Stack>
             <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen name="detail" options={{ headerShown: false }} />
 
             {/* <Stack.Screen name="[id]" options={{ headerShown: false }} /> */}
         </Stack>
