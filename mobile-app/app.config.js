b698711bfb55cf8c090e2d43bff5dbbbebd4e9c2
Chau Phuc <chauphuc09090@gmail.com>
2026-06-07 15:44:13 +0700

    chore: resize splash screen

--- mobile-app/app.config.js
+++ mobile-app/app.config.js
@@ -44,8 +44,7 @@ export default {
         "expo-splash-screen",
         {
           "image": "./assets/images/splash-icon.png",
-          "imageWidth": 200,
-          "resizeMode": "contain",
+          "resizeMode": "cover",
           "backgroundColor": "#ffffff",
           "dark": {
             "backgroundColor": "#000000"
